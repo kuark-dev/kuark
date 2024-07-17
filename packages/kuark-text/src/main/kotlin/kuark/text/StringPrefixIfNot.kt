3424add289097fe2988b22c0a11a8c8764f28927
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-07-17 15:28:09 +0900

    refactor: `inline` 속성 추가

--- packages/kuark-text/src/main/kotlin/kuark/text/StringPrefixIfNot.kt
+++ packages/kuark-text/src/main/kotlin/kuark/text/StringPrefixIfNot.kt
@@ -3,4 +3,4 @@ package kuark.text
 /**
  * 현재 문자열의 [prefix]가 존재하지 않는 경우 [prefix]를 추가한 새 문자열을 반환하고, 그렇지 않으면 원래 문자열을 반환한다
  */
-fun String.prefixIfNot(prefix: String): String = this.prefixIf(prefix) { this.startsWith(prefix).not() }
+inline fun String.prefixIfNot(prefix: String): String = this.prefixIf(prefix) { this.startsWith(prefix).not() }
--- packages/kuark-text/src/main/kotlin/kuark/text/StringSuffixIfNot.kt
+++ packages/kuark-text/src/main/kotlin/kuark/text/StringSuffixIfNot.kt
@@ -4,4 +4,4 @@ package kuark.text
 /**
  * 현재 문자열의 [suffix]가 존재하지 않는 경우 [suffix]를 추가한 새 문자열을 반환하고, 그렇지 않으면 원래 문자열을 반환한다
  */
-fun String.suffixIfNot(suffix: String): String = this.suffixIf(suffix) { this.endsWith(suffix).not() }
+inline fun String.suffixIfNot(suffix: String): String = this.suffixIf(suffix) { this.endsWith(suffix).not() }
